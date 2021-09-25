b32c9b8aae7e9838523c5480e42c03499fee0588
Daniel Marriner <danielmarriner@anachronistic-tech.co.uk>
2021-09-25 19:41:11 +0300

    Initial Commit

---
+++ Shared/MusicalStocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicalStocksApp.swift
+//  Shared
+//
+//  Created by Daniel Marriner on 25/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct MusicalStocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
